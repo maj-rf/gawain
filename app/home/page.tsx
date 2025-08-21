a43350d8ae6b9590464b48eb74dc36ae96416589
maj-rf <rmkfermin@gmail.com>
2025-08-21 09:32:20 +0800

    style: create base card style for boards in workspaces

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,24 +1,39 @@
 import Navbar from '@/components/navbar';
 import { getSession } from '@/lib/auth';
 import { redirect } from 'next/navigation';
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+
+const boards: Array<{ id: string; title: string }> = [
+  {
+    id: '1',
+    title:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores, odio.',
+  },
+  { id: '2', title: 'hello' },
+  { id: '3', title: 'whatever' },
+  { id: '4', title: 'i misss your tan skin' },
+  { id: '5', title: 'your sweet smile' },
+];
+
 function HomePageWorkspaces() {
-  const boards: Array<{ id: string; title: string }> = [
-    { id: '1', title: 'hello' },
-    { id: '2', title: 'hello' },
-    { id: '3', title: 'hello' },
-    { id: '4', title: 'hello' },
-    { id: '5', title: 'hello' },
-  ];
   return (
-    <section>
-      <h1>Your Workspaces</h1>
-      <div className="grid grid-cols-3">
-        <Button>Create new board</Button>
+    <section className="grid gap-2">
+      <h1 className="uppercase font-bold">Your Workspaces</h1>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(160px,1fr))] gap-4">
+        <div className="bg-accent border rounded-xl shadow-sm flex items-center justify-center">
+          Create new board
+        </div>
         {boards.map((b) => (
           <Link href={`/boards/${b.id}`} key={b.id}>
-            {b.title}
+            <Card className="p-0 gap-0 overflow-hidden">
+              <div className="h-15 bg-radial-[at_25%_25%] from-indigo-400 to-purple-500 to-95%"></div>
+              <CardHeader className="py-3">
+                <CardTitle className="font-normal truncate">
+                  {b.title}
+                </CardTitle>
+              </CardHeader>
+            </Card>
           </Link>
         ))}
       </div>
@@ -32,7 +47,7 @@ export default async function HomePage() {
   return (
     <div>
       <Navbar user={session.user} />
-      <main>
+      <main className="px-4 py-2">
         <HomePageWorkspaces />
       </main>
     </div>
