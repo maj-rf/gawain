fe6e98814a3f97d4936972f413c14320306af0f6
maj-rf <rmkfermin@gmail.com>
2025-09-04 11:08:03 +0800

    fix: migrate db to supabase, update createboard form and action

--- components/create-board-form.tsx
+++ components/create-board-form.tsx
@@ -2,7 +2,7 @@
 import { cn } from '@/lib/utils';
 import { Input } from '@/components/ui/input';
 import { Button } from './ui/button';
-import { useActionState } from 'react';
+import { useActionState, useTransition } from 'react';
 import { handleCreateBoard } from '@/lib/actions/board-actions';
 import { CreateBoardSchema } from '@/lib/schemas';
 import { State } from '@/types/types';
@@ -12,6 +12,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
 
 export function CreateBoardForm({ className }: React.ComponentProps<'form'>) {
+  const initialState: State = { message: '', success: false };
+  // TODO: how to mix RHF & useActionState???
+  const [state, formAction, isPending] = useActionState(handleCreateBoard, initialState);
+  const [, startTransition] = useTransition();
   const form = useForm<z.infer<typeof CreateBoardSchema>>({
     resolver: zodResolver(CreateBoardSchema),
     defaultValues: {
@@ -19,17 +23,18 @@ export function CreateBoardForm({ className }: React.ComponentProps<'form'>) {
     },
   });
 
-  function onSubmit() {
-    // TODO: put formAction here???
-    console.log('submitted');
-  }
-
-  const initialState: State = { message: '', success: false };
-  // TODO: how to mix RHF & useActionState???
-  const [state, formAction, isPending] = useActionState(handleCreateBoard, initialState);
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className={cn('grid items-start gap-6', className)}>
+      <form
+        action={formAction}
+        onSubmit={form.handleSubmit((_, e) => {
+          startTransition(() => {
+            const formData = new FormData(e?.target);
+            formAction(formData);
+          });
+        })}
+        className={cn('grid items-start gap-6', className)}
+      >
         <FormField
           control={form.control}
           name="title"
@@ -44,7 +49,9 @@ export function CreateBoardForm({ className }: React.ComponentProps<'form'>) {
           )}
         ></FormField>
 
-        <Button type="submit">Submit</Button>
+        <Button type="submit" disabled={isPending}>
+          Submit
+        </Button>
       </form>
     </Form>
   );
--- lib/actions/board-actions.ts
+++ lib/actions/board-actions.ts
@@ -5,22 +5,26 @@ import { createBoard } from '../queries';
 import { requireAuth } from '../data';
 import { CreateBoardSchema } from '../schemas';
 import { State } from '@/types/types';
+import { revalidatePath } from 'next/cache';
 
 type CreateBoardResponse = {
   success: boolean;
   message: string;
 };
 
-export async function handleCreateBoard(prevState: State, data: { title: string }): Promise<CreateBoardResponse> {
+export async function handleCreateBoard(prevState: State, formData: FormData): Promise<CreateBoardResponse> {
   try {
     const user = await requireAuth();
-    const validate = CreateBoardSchema.safeParse({ title: data.title });
+    const form = Object.fromEntries(formData);
+    const parsedForm = CreateBoardSchema.safeParse(form);
     // server-side validation
-    if (!validate.success) return { success: false, message: z.prettifyError(validate.error) };
+    if (!parsedForm.success) return { success: false, message: z.prettifyError(parsedForm.error) };
+    await createBoard({ title: parsedForm.data.title, ownerId: user.id });
+    revalidatePath('/');
 
-    await createBoard({ title: data.title, ownerId: user.id });
     return { success: true, message: notifs.BOARD.CREATE_SUCCESS };
-  } catch {
+  } catch (error) {
+    console.log(error);
     return { success: false, message: 'Something went wrong' };
   }
 }
